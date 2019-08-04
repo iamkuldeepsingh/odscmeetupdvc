14b6d27fb0eccf821d973fb276473749f49d17be
Kuldeep Singh <Kuldeep_Singh@mckinsey.com>
2019-08-04 18:22:16 +0530

    Final Commit

---
+++ datapush.sh
@@ -0,0 +1,4 @@
+dvc remote add -d upstream /tmp/dvctest
+dvc remost list 
+git status -s
+dvc push 
---
+++ pullrepro.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/dmpetrov/new_tag_classifier.git
+dvc pull
+dvc repro
